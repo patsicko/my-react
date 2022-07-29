1a0702096f499681ca67fca763abbffdb00239a9
patsicko <patsicko@g>
2022-07-29 10:48:54 +0200

    React

--- src/index.js
+++ src/index.js
@@ -1,28 +1,29 @@
 import React from 'react';
+import { useState } from 'react';
 import { render } from 'react-dom';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-function MyAddress(props){
-  return <li>I am {props.name}</li>
+const MyColor=()=>{
+
+
+  const[color,setColor]=useState("green");
+
+  return (<>
+  <p>my favorite color is {color}</p>
+
+  <button type="button" onClick={()=>{setColor("blue")}}>Blue</button> <br/><br/>
+  <button type="button" onClick={()=>{setColor("red")}}>Red</button><br/><br/>
+  <button type="button" onClick={()=>{setColor("orange")}} >Orange</button><br/><br/>
+  <button type="button" onClick={()=>{setColor("yellow")}} >Orange</button><br/><br/>
+  <button type="button" onClick={()=>{setColor("gray")}} >Orange</button><br/><br/>
+  </>);
+
+
 }
 
 
-function TrueMe(){
-
-const me=["Patsicko","PatsiTech","CEO"];
-
-return <>
-<ul>
-  
-{me.map(item=><MyAddress name={item} />)}
-</ul>
-</>
-
-}
-
-
-ReactDOM.createRoot(document.getElementById("root")).render(<TrueMe/>)
+ReactDOM.createRoot(document.getElementById("root")).render(<MyColor/>)
 
 
 
